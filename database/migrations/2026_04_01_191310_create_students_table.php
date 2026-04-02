71db34c60977f7bf596baaad9bdd2f8c921aa8c1
Rodrigo Tunala Rezende <rodrigotunala@gmail.com>
2026-04-02 14:59:21 -0300

    feat:add database schema for students, reponsibles and measurements

---
+++ database/migrations/2026_04_01_191310_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('birth_date');
+            $table->enum('shift', ['am', 'pm']);
+            $table->string('phone', 20)->nullable();
+            $table->string('address');
+            $table->string('neighborhood');
+            $table->string('school');
+            $table->string('school_situation');
+            $table->string('school_year');
+            $table->foreignId('responsible_id')->constrained('responsibles')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+        Schema::dropIfExists('students');
+    }
+};
